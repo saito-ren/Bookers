93de6a37dd87d6731b5806039bee39c623ad33b2
vagrant <vagrant@localhost.localdomain>
2020-05-13 14:07:04 +0000

    edit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book.id)
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
   end
+
+  def top
+  end
+
+  private book_params
+  params.require(:book).permit(:title,:body)
 end
